f18eccbde5e4a8c0cae2ff79599db573cd38a7f9
hechao <info@electrodragon.com>
2020-10-06 19:43:17 +0800

    update

--- Sketchbook/interface-IIC-scan/interface-IIC-scan.ino
+++ Sketchbook/interface-IIC-scan/interface-IIC-scan.ino
@@ -1,13 +1,15 @@
-//I2C device found at address 0x3C  !
-//I2C device found at address 0x75  !
+//I2C device found at address 0x3C  ! 
 
+//I2C device found at address 0x75  ! // IP5306
+
+//I2C device found at address 0x76  !   // BME280
 
 #include <Wire.h>
 
 
 void setup()
 {
-  Wire.begin();
+  Wire.begin(4,13);
 
   Serial.begin(115200);
   Serial.println("\nI2C Scanner");
