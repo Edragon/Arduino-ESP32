ac0b48c5fa0ddaf1fb2b95692e8fc74c9b6b9233
hechaogm <hechaogm@gmail.com>
2023-04-26 16:24:54 +0800

    update

--- Sketchbook/BSP/NWI1199/NWI1199-arduino/NWI1199-arduino.ino
+++ Sketchbook/BSP/NWI1199/NWI1199-arduino/NWI1199-arduino.ino
@@ -14,10 +14,10 @@
 #undef ETH_CLK_MODE
 #endif
 
-#define ETH_CLK_MODE    ETH_CLOCK_GPIO0_IN
+#define ETH_CLK_MODE    ETH_CLOCK_GPIO17_OUT
 
 // Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define ETH_POWER_PIN   -1
+#define ETH_POWER_PIN   0
 
 // Type of the Ethernet PHY (LAN8720 or TLK110)
 #define ETH_TYPE        ETH_PHY_LAN8720
