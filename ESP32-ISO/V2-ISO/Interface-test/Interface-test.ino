8ae2f663771578e9b56c719e7e693dd34e1a6321
hechaogm <hechaogm@gmail.com>
2023-10-17 17:37:10 +0800

    esp-iso

--- ESP32-ISO/V2-ISO/Interface-test/Interface-test.ino
+++ ESP32-ISO/V2-ISO/Interface-test/Interface-test.ino
@@ -5,32 +5,40 @@
 // a single 8N1 word, are on the basis that any char written to the loopback SoftwareSerial adapter gets read
 // before another write is performed. Block writes with a size greater than 1 would usually fail.
 
-#define UART1_RX 21
-#define UART1_TX 22
+#define U1_RX 21
+#define U1_TX 22
 
-SoftwareSerial UART1 (UART1_RX, UART1_TX);
+#define U2_RX 16
+#define U2_TX 17
 
+
+SoftwareSerial U1 (U1_RX, U1_TX);
+SoftwareSerial U2 (U2_RX, U2_TX);   // rx tx 
+
+#define activeUART U2
 
 void setup() {
   Serial.begin(9600);
   delay(500);
-  //U1.begin(9600, SWSERIAL_8N1, U1R, U1T, false, 95, 11);
-  UART1.begin(9600);
-  delay(500);
   Serial.println("\nSoftware serial test started");
   delay(500);
-  UART1.println("AT\r\n");
+  
+  //U1.begin(9600, SWSERIAL_8N1, U1R, U1T, false, 95, 11);
+  activeUART.begin(9600);
   delay(500);
+
 }
 
+
+
 void loop() {
-  if (UART1.available()) {
-    Serial.write(UART1.read());
+  if (activeUART.available()) {
+    Serial.write(activeUART.read());
     yield();
   }
 
   if (Serial.available()) { // note
-    UART1.write(Serial.read());
+    activeUART.write(Serial.read());
     yield();
   }
 
